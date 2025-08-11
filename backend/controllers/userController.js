c5c4e1ddade4fba2aae1e36d8a2d43379654e857
ice <icesahachon333@gmail.com>
2025-08-11 22:25:54 +0700

    register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,12 @@
-import userModel from "../models/userModel";
+import validator from 'validator';
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
+import userModel from "../models/userModel.js";
 
+
+const createToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
 
 // Routes for user login
 const loginUser = async (req, res) => {
@@ -19,8 +26,34 @@ const registerUser = async (req, res) => {
             return res.json({success:false, message:"User already exists"})
         }
 
-    } catch (error) {
+        // validating email format & strong password
+        if (!validator.isEmail(email)) {
+            return res.json({success:false, message:"Please enter a valid email"})
+        }
+        if (password.length < 8) {
+            return res.json({success:false, message:"Please enter a strong password"})
+        }
 
+        // hashing user password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save()
+
+        const token = createToken(user._id)
+
+        res.json({success:true,token})
+
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
     }
 }
 
