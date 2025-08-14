85077e47e9700d0e05ac7476f8e7a529a2a4b77a
ice <icesahachon333@gmail.com>
2025-08-14 22:21:55 +0700

    addProduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,8 +2,24 @@
 
 
 // function for add product
-const addProducts = async (req, res) => {
-    
+const addProduct = async (req, res) => {
+    try {
+
+        const { name, description, price, category, subCategory, sizes, bestseller } = req.body
+
+        const image1 = req.files.image1[0]
+        const image2 = req.files.image2[0]
+        const image3 = req.files.image3[0]
+        const image4 = req.files.image4[0]
+
+        console.log(name, description, price, category, subCategory, sizes, bestseller)
+        console.log(image1,image2,image3,image4)
+        
+        res.json({})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 
 }
 
@@ -13,7 +29,7 @@ const listProducts = async (req, res) => {
 }
 
 // function for removing product
-const removeProducts = async (req, res) => {
+const removeProduct = async (req, res) => {
 
 }
 
@@ -22,5 +38,5 @@ const singleProduct = async (req, res) => {
 
 }
 
-export { addProducts, listProducts, removeProducts, singleProduct }
+export { listProducts, addProduct, removeProduct, singleProduct }
 
