2fa20cbeaeab4cae3f6655718a168884db8b4041
Ice <icesahachon333@gmail.com>
2025-08-24 02:20:57 +0700

    add product finish

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import { backendUrl } from '../App'
+import { toast } from 'react-toastify'
 
-const Add = () => {
+const Add = ({token}) => {
 
   const [image1,setImage1] = useState(false)
   const [image2,setImage2] = useState(false)
@@ -12,11 +15,11 @@ const Add = () => {
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("Pork");
-  const [subCategory, setSubCategory] = useState("pack  ");
+  const [subCategory, setSubCategory] = useState("pack");
   const [bestseller, setBestseller] = useState(false);
   const [sizes, setSizes] = useState("");
 
-  const onSubmitHandler = async () => {
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
 
     try {
@@ -30,9 +33,31 @@ const Add = () => {
       formData.append("subCategory",subCategory)
       formData.append("bestseller",bestseller)
       formData.append("sizes",sizes)
+
+      image1 && formData.append("image1",image1)
+      image2 && formData.append("image2",image2)
+      image3 && formData.append("image3",image3)
+      image4 && formData.append("image4",image4)
+
+      const response = await axios.post(backendUrl + "/api/product/add",formData,{headers:{token}})
+
+      if (response.data.success) {
+        toast.success(response.data.message)
+        setName('')
+        setDescription('')
+        setImage1(false)
+        setImage2(false)
+        setImage3(false)
+        setImage4(false)
+        setPrice('')
+        setSizes('')
+      } else {
+        toast.error(response.data.message)
+      }
       
     } catch (error) {
-      
+      console.log(error);
+      toast.error(error.message)
     }
   }
 
